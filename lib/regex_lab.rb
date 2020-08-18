25fdf7fc4341e538cfddf9731503ff494e46f457
John Stassen <john.stassen@gmail.com>
2020-08-18 23:51:23 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  word.match? /\A[aeiou]/i
+  word.match? /\b[aeiou]/i
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -11,7 +11,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(\A[A-Z].+[.]\z)
+  text.scan(^[A-Z].+[.]\z)
 end
 
 def valid_phone_number?(phone)
